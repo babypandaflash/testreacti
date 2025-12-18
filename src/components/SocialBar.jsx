605e2d1dc16d708454b6de0c0b4c47da6daa686b
BabyPanda Flash <babypandaflash@gmail.com>
2025-12-18 18:21:47 +0700

    Create SocialBar.jsx

---
+++ src/components/SocialBar.jsx
@@ -0,0 +1,21 @@
+export default function SocialBar() {
+  return (
+    <div className="social-bar">
+      <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+        <i className="fa-brands fa-x-twitter"></i>
+      </a>
+      <a href="https://discord.com/" target="_blank" rel="noopener noreferrer">
+        <i className="fa-brands fa-discord"></i>
+      </a>
+      <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
+        <i className="fa-brands fa-instagram"></i>
+      </a>
+      <a href="https://t.me/" target="_blank" rel="noopener noreferrer">
+        <i className="fa-brands fa-telegram"></i>
+      </a>
+      <a href="https://youtube.com/" target="_blank" rel="noopener noreferrer">
+        <i className="fa-brands fa-youtube"></i>
+      </a>
+    </div>
+  )
+}
