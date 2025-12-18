f18edadd662ffdf72ac9a595e9d2c678a43d1d5a
BabyPanda Flash <babypandaflash@gmail.com>
2025-12-18 15:35:05 +0700

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import EarlyAccess from './EarlyAccess';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EarlyAccess />
+  </React.StrictMode>
+);
